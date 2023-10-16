94656852df7a924fa0254a2fb55667b261bc4361
Wajeeh Syed <wsyed019@uottawa.ca>
2023-10-15 20:42:52 -0400

    commit all activities

--- Android/app/src/main/java/com/example/hamsapplication/welcomeScreen.java
+++ Android/app/src/main/java/com/example/hamsapplication/welcomeScreen.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class welcomeScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_welcome_screen);
     }
 }
