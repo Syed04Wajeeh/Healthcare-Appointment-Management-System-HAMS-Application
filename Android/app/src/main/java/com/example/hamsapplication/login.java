f25ac4f31d083dde8275680e4cfbe9fab6cf7493
Wajeeh Syed <wsyed019@uottawa.ca>
2023-10-16 16:16:24 -0400

    Working buttons!

--- Android/app/src/main/java/com/example/hamsapplication/login.java
+++ Android/app/src/main/java/com/example/hamsapplication/login.java
@@ -2,7 +2,9 @@ package com.example.hamsapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -11,7 +13,8 @@ public class login extends AppCompatActivity {
     private EditText username;
     private EditText password;
     private Button login;
-    private Button register;
+    private Button patientRegister;
+    private Button doctorRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +22,32 @@ public class login extends AppCompatActivity {
         setContentView(R.layout.login);
 
         username = findViewById(R.id.usernameInput);
-        username = findViewById(R.id.password);
-        login = findViewById(R.id.loginButton);
-        login = findViewById(R.id.patientRegisterButton);
+        password = findViewById(R.id.passwordInput);
 
+        login = (Button) findViewById(R.id.loginButton);
+        patientRegister = (Button)findViewById(R.id.patientRegisterButton);
+        doctorRegister = (Button) findViewById(R.id.doctorRegisterButton);
 
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this, welcomeScreen.class);
+                startActivity(intent);
+            }
+        });
+        patientRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this, patientRegister.class);
+                startActivity(intent);
+            }
+        });
+        doctorRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this, doctorRegister.class);
+                startActivity(intent);
+            }
+        });
     }
 }
