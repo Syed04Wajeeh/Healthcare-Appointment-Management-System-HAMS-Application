0f1a0f03ba14fbce0f5c82c6b4d88add60a4b404
YuYang Liu <yiu863@uottawa.ca>
2023-10-16 21:29:32 -0400

    i love doing this

--- Android/app/src/main/java/com/example/hamsapplication/doctorRegister.java
+++ Android/app/src/main/java/com/example/hamsapplication/doctorRegister.java
@@ -3,12 +3,56 @@ package com.example.hamsapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class doctorRegister extends AppCompatActivity {
+
+    private EditText firstName;
+    private EditText lastName;
+    private EditText email;
+    private EditText password;
+    private EditText phoneNumber;
+    private EditText address;
+    private EditText employeeNumber;
+    private EditText specialties;
+    private Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_register);
+
+        registerButton = (Button) findViewById(R.id.register);
+
+        firstName = (EditText) findViewById(R.id.doctorFirstName);
+        lastName = (EditText) findViewById(R.id.doctorLastName);
+        email = (EditText) findViewById(R.id.doctorEmailUsername);
+        password = (EditText) findViewById(R.id.doctorAccountPassword);
+        phoneNumber = (EditText) findViewById(R.id.doctorPhoneNumber);
+        address = (EditText) findViewById(R.id.doctorAddress);
+        employeeNumber = (EditText) findViewById(R.id.doctorNumber);
+        specialties = (EditText) findViewById(R.id.doctorSpecialty);
+
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String userFirstName = firstName.getText().toString();
+                String userLastName = lastName.getText().toString();
+                String userEmail = email.getText().toString();
+                String userPassword = password.getText().toString();
+                String userPhoneNumber = phoneNumber.getText().toString();
+                String userAddress = address.getText().toString();
+                String userEmployeeNumber = address.getText().toString();
+                String userSpecialties = address.getText().toString();
+
+                doctorInformation doctor = new doctorInformation(userFirstName, userLastName, userEmail,
+                        userPassword, userPhoneNumber, userAddress, userEmployeeNumber, userSpecialties);
+                generalInformation.addToCollection(doctor);
+            }
+        });
     }
 }
--- Android/app/src/main/java/com/example/hamsapplication/patientRegister.java
+++ Android/app/src/main/java/com/example/hamsapplication/patientRegister.java
@@ -3,12 +3,52 @@ package com.example.hamsapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class patientRegister extends AppCompatActivity {
+
+    private EditText firstName;
+    private EditText lastName;
+    private EditText email;
+    private EditText password;
+    private EditText phoneNumber;
+    private EditText address;
+    private EditText healthCardNumber;
+    private Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_register);
+
+        registerButton = (Button) findViewById(R.id.register);
+
+        firstName = (EditText) findViewById(R.id.patientFirstName);
+        lastName = (EditText) findViewById(R.id.patientLastName);
+        email = (EditText) findViewById(R.id.patientEmailUsername);
+        password = (EditText) findViewById(R.id.patientAccountPassword);
+        phoneNumber = (EditText) findViewById(R.id.patientPhoneNumber);
+        address = (EditText) findViewById(R.id.patientAddress);
+        healthCardNumber = (EditText) findViewById(R.id.patientHealthCardNumber);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String userFirstName = firstName.getText().toString();
+                String userLastName = lastName.getText().toString();
+                String userEmail = email.getText().toString();
+                String userPassword = password.getText().toString();
+                String userPhoneNumber = phoneNumber.getText().toString();
+                String userAddress = address.getText().toString();
+                String userHealthCardNumber = healthCardNumber.getText().toString();
+
+                patientInformation patient = new patientInformation(userFirstName, userLastName, userEmail,
+                        userPassword, userPhoneNumber, userAddress, userHealthCardNumber);
+                generalInformation.addToCollection(patient);
+            }
+        });
     }
 }
