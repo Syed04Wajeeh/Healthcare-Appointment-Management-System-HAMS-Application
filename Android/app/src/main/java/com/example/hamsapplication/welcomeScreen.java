b822bbfb28dfa92d37e94f22e702bf87ddac83b6
YuYang Liu <yiu863@uottawa.ca>
2023-10-16 22:23:03 -0400

    final commit

--- Android/app/src/main/java/com/example/hamsapplication/welcomeScreen.java
+++ Android/app/src/main/java/com/example/hamsapplication/welcomeScreen.java
@@ -2,13 +2,28 @@ package com.example.hamsapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class welcomeScreen extends AppCompatActivity {
+
+    private Button logOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        logOut = (Button) findViewById(R.id.logOff);
+
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(welcomeScreen.this, login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
