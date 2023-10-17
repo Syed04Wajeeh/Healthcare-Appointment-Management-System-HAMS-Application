4c00d61690d75d9e2c4bba4b8b6d9e4d3f49f841
Wajeeh Syed <wsyed019@uottawa.ca>
2023-10-16 20:50:57 -0400

    6 29

---
+++ Android/app/src/main/java/com/example/hamsapplication/patientInformation.java
@@ -0,0 +1,9 @@
+package com.example.hamsapplication;
+
+public class patientInformation extends generalInformation {
+    String healthNumber;
+    protected patientInformation(String username, String password, String firstName, String lastName, String phoneNumber, String address, String healthNumber) {
+        super(username, password, firstName, lastName, phoneNumber, address);
+        this.healthNumber = healthNumber;
+    }
+}
