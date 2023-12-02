0de278f6d7914000f61855da6b202b49282bf26f
Wajeeh Syed <wsyed019@uottawa.ca>
2023-12-02 16:39:26 -0500

    Grrr

---
+++ Android/app/src/main/java/com/example/hamsapplication/PastAppointmentPatient.java
@@ -0,0 +1,14 @@
+package com.example.hamsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PastAppointmentPatient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_past_appointment_patient);
+    }
+}
